5336bd957e7c21b5af327b45425c81850927fe97
Miikka Koskinen <miikka@metosin.fi>
2017-01-05 11:13:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This repository demonstrates a problem with
+[Virgil](https://github.com/ztellman/virgil) and
+[Whidbey](https://github.com/greglook/whidbey). Run `lein repl` - you'll get a
+stacktrace instead of a REPL.
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject whidbey-virgil-problem "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :java-source-paths ["java-src"]
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-virgil "0.1.5"]
+            [mvxcvi/whidbey "1.3.1"]])
